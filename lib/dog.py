6e1d0d2e6678ac256049680d74ca52eb5a3593bf
email <bethkimaku@gmail.com>
2023-06-10 21:52:42 +0300

    commpleted assignment

--- lib/dog.py
+++ lib/dog.py
@@ -4,5 +4,86 @@ CONN = sqlite3.connect('lib/dogs.db')
 CURSOR = CONN.cursor()
 
 class Dog:
-    
-    pass
+    def __init__(self, name, breed):
+        self.id = None
+        self.name = name
+        self.breed = breed
+
+    @classmethod
+    def create_table(cls):
+        sql = """
+            CREATE TABLE IF NOT EXISTS dogs (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                breed TEXT
+            )
+        """
+        CURSOR.execute(sql)
+
+    @classmethod
+    def drop_table(cls):
+        sql = """
+        DROP TABLE IF EXISTS dogs
+           """
+
+    def save(self):
+        if self.id is None:
+            sql = """
+            INSERT INTO dogs (name, breed)
+            VALUES (?, ?)
+            """
+            CURSOR.execute(sql, (self.name, self.breed))
+            CONN.commit()
+            self.id = CURSOR.lastrowid
+        else:
+            self.update()
+
+    def update(self):
+        sql = """
+        UPDATE dogs
+        SET name=?, breed=?
+        WHERE id=?
+        """
+        CURSOR.execute(sql, (self.name, self.breed, self.id))
+        CONN.commit()
+
+    @classmethod
+    def create(cls, name, breed):
+        dog = Dog(name, breed)
+        dog.save()
+        return dog
+
+    @classmethod
+    def new_from_db(cls, row):
+        name,breed,id =row
+        return cls(name, breed, id)
+
+    @classmethod
+    def get_all(cls):
+        sql = "SELECT * FROM dogs"
+        CURSOR.execute(sql)
+        rows = CURSOR.fetchall()
+        return [Dog.new_from_db(row) for row in rows]
+
+    @classmethod
+    def find_by_name(cls, name):
+        sql = "SELECT * FROM dogs WHERE name=?"
+        CURSOR.execute(sql, (name,))
+        row = CURSOR.fetchone()
+        if row:
+            return Dog.new_from_db(row)
+
+    @classmethod
+    def find_by_id(cls, id):
+        sql = "SELECT * FROM dogs WHERE id=?"
+        CURSOR.execute(sql, (id,))
+        row = CURSOR.fetchone()
+        if row:
+            return Dog.new_from_db(row)
+
+    @classmethod
+    def find_or_create_by(cls, name, breed):
+        dog = Dog.find_by_name(name)
+        if dog is None:
+            dog = Dog.create(name, breed)
+        return dog
